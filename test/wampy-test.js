329cfbb4c72a2a2ed1cdf538c71489dc002e00a1
Konstantin Burkalev <KSDaemon@yandex.ru>
2015-04-12 20:44:58 +0300

    Started to describe tests

--- test/wampy-test.js
+++ test/wampy-test.js
@@ -1,5 +1,38 @@
 /**
  * Project: wampy.js
- * User: kostik
+ * User: KSDaemon
  * Date: 07.04.15
  */
+
+var expect = require('chai').expect,
+    WebSocket = require('./fake-ws'),
+    Wampy = require('./../src/wampy');
+
+describe('Wampy.js', function () {
+    var wampy = new Wampy('/ws/', { realm: 'AppRealm' });
+
+    before(function () {
+
+    })
+
+    describe('Constructor', function () {
+
+        it('allows to connect on instantiation if all required options specified');
+
+        it('disallows to connect on instantiation without url');
+
+        it('disallows to connect on instantiation without realm');
+
+        it('allows to set different options on instantiation');
+
+    });
+
+    describe('Instance', function () {
+
+        it('allows to set different options');
+
+        it('allows to get different options');
+
+    });
+
+})
