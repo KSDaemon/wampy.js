671cc28a5091426c371d0a2f22cee20dcde02eef
Konstantin Burkalev <KSDaemon@yandex.ru>
2015-04-12 20:44:40 +0300

    First draft of Websocket mock

--- test/fake-ws.js
+++ test/fake-ws.js
@@ -1,5 +1,38 @@
 /**
  * Project: wampy.js
- * User: kostik
+ * User: KSDaemon
  * Date: 07.04.15
  */
+
+;(typeof define === 'function' ? function (m) { define('Wampy', m); } :
+    typeof exports === 'object' ? function (m) { module.exports = m(); } :
+    function (m) { this.Wampy = m(); }
+)(function () {
+
+    var WebSocket = function (url, protocols) {
+        this.url = url;
+        this.protocols = protocols;
+        this.binaryType = 'arraybuffer';
+
+        this.onclose = null;
+        this.onerror = null;
+        this.onmessage = null;
+        this.onopen = null;
+
+        this.protocol = '';
+
+        this.readyState = 3;    // Closed
+
+    };
+
+    WebSocket.prototype.close = function (code, reason) {
+
+    };
+
+    WebSocket.prototype.send = function (data) {
+
+    };
+
+    return WebSocket;
+
+});
