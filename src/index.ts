5985f183b91b0b38ec01c52fb291dea35ac18fc9
MakeShiftArtist <lrmcbride4@gmail.com>
2022-04-11 00:31:41 -0500

    Improved exports to add helpers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import Wampy from "./wampy";
+import {
+    WAMP_ERROR_MSG,
+    WAMP_MSG_SPEC,
+    isNode,
+    ALLOWED_BINARY_TYPES,
+} from "./constants";
+import JsonSerializer from "./serializers/JsonSerializer";
+import MsgpackSerializer from "./serializers/MsgpackSerializer";
+
 export default Wampy;
-export { Wampy };
+
+export {
+    Wampy,
+    JsonSerializer,
+    MsgpackSerializer,
+    WAMP_ERROR_MSG,
+    WAMP_MSG_SPEC,
+    ALLOWED_BINARY_TYPES,
+    isNode,
+};
