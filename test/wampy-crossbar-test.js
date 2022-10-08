2365f4d6820d989165b89ef9f23e63914610fa9d
Konstantin Burkalev <KSDaemon@gmail.com>
2022-10-08 11:22:33 +0300

    Fixed crossbar tests

--- test/wampy-crossbar-test.js
+++ test/wampy-crossbar-test.js
@@ -10,60 +10,62 @@ describe('Wampy.js with Crossbar', function () {
 
     it('Works with Json serializer', function (done) {
 
-        const server = new Wampy('ws://localhost:8888/test', {
+        const wampy = new Wampy('ws://localhost:8888/test', {
             realm: 'realm1',
             serializer: new JsonSerializer(),
-            ws: w3cwebsocket,
-            onConnect: () => {
-                server.register('sayhello', {
-                    rpc: () => {
-                        return { argsList: 'hello' };
-                    },
-                    onSuccess: () => {
-                        const client = new Wampy('ws://localhost:8888/test', {
-                            realm: 'realm1',
-                            serializer: new JsonSerializer(),
-                            ws: w3cwebsocket,
-                            onConnect: () => {
-                                client.call('sayhello', [], result => {
-                                    expect(result.argsList.shift()).to.equal('hello');
-                                    done();
-                                });
-                            }
-                        });
-                    }
+            ws: w3cwebsocket
+        });
+
+        wampy.connect().then(() => {
+            wampy.register('sayhello', {
+                rpc: () => {
+                    return { argsList: 'hello' };
+                }
+            }).then(() => {
+                const client = new Wampy('ws://localhost:8888/test', {
+                    realm: 'realm1',
+                    serializer: new JsonSerializer(),
+                    ws: w3cwebsocket
                 });
-            }
+                client.connect().then(() => {
+                    client.call('sayhello', []).then((result) => {
+                        expect(result.argsList.shift()).to.equal('hello');
+                        done();
+                    });
+
+                });
+            });
         });
 
     });
 
     it('works with Msgpack serialization', function (done) {
 
-        const server = new Wampy('ws://localhost:8888/test', {
+        const wampy = new Wampy('ws://localhost:8888/test', {
             realm: 'realm1',
             serializer: new MsgpackSerializer(),
-            ws: w3cwebsocket,
-            onConnect: () => {
-                server.register('sayhello2', {
-                    rpc: () => {
-                        return { argsList: 'hello' };
-                    },
-                    onSuccess: () => {
-                        const client = new Wampy('ws://localhost:8888/test', {
-                            realm: 'realm1',
-                            serializer: new MsgpackSerializer(),
-                            ws: w3cwebsocket,
-                            onConnect: () => {
-                                client.call('sayhello2', [], result => {
-                                    expect(result.argsList.shift()).to.equal('hello');
-                                    done();
-                                });
-                            }
-                        });
-                    }
+            ws: w3cwebsocket
+        });
+
+        wampy.connect().then(() => {
+            wampy.register('sayhello2', {
+                rpc: () => {
+                    return { argsList: 'hello' };
+                }
+            }).then(() => {
+                const client = new Wampy('ws://localhost:8888/test', {
+                    realm: 'realm1',
+                    serializer: new MsgpackSerializer(),
+                    ws: w3cwebsocket
                 });
-            }
+                client.connect().then(() => {
+                    client.call('sayhello2', []).then((result) => {
+                        expect(result.argsList.shift()).to.equal('hello');
+                        done();
+                    });
+
+                });
+            });
         });
 
     });
