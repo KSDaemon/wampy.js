c165474c441e19b8f57432a958ccb47dbabad16a
Konstantin Burkalev <KSDaemon@gmail.com>
2022-10-20 22:54:36 +0300

    Added serializers to exports section in package.json

--- src/wampy.js
+++ src/wampy.js
@@ -37,7 +37,7 @@ class Wampy {
          * @type {string}
          * @private
          */
-        this.version = 'v7.0.0-rc3';
+        this.version = 'v7.0.0-rc4';
 
         /**
          * WS Url
